4074881dfefa6f7fc70b0effb9c202c8165ad154
angrykoala <angrykoala@outlook.es>
2021-10-26 23:18:52 +0100

    Orbit info

--- autoload/HUD.gd
+++ autoload/HUD.gd
@@ -2,28 +2,46 @@ extends CanvasLayer
 
 
 onready var planet_name_label:Label=$PlanetInfo/PlanetName
+
 onready var ships_label:Label=$PlanetInfo/ShipsLabel
-onready var reosurces_label:Label=$PlanetInfo/ResourcesLabel
+onready var resources_label:Label=$PlanetInfo/ResourcesLabel
 onready var planet_info:Panel=$PlanetInfo
 
+onready var orbit_info:Panel=$OrbitInfo
+onready var orbit_name_label:Label=$OrbitInfo/OrbitName
+onready var orbit_ships_label:Label=$OrbitInfo/ShipsLabel
+
 var planet:Planet
+var orbit:Orbit
 
 func _ready():
-	hide_planet_info()
+	reset_info()
 
 func display_planet_info(planet: Planet):
 	self.planet=planet
+	self.orbit=null
+
+func display_orbit_info(orbit:Orbit):
+	print("DISPLAY ORBIT INFO")
+	self.orbit=orbit
+	self.planet=null
 
 func _process(delta: float) -> void:
+	reset_info()
 	if planet:
 		planet_name_label.text=planet.planet_name
 		planet_info.visible=true
 		ships_label.text="Ships: "+ String(planet.ships.size())
-		reosurces_label.text="Resources: "+ String(planet.resources) + " (+" + String(planet.production_rate)+ ")"
+		resources_label.text="Resources: "+ String(planet.resources) + " (+" + String(planet.production_rate)+ ")"
+	
+	elif orbit:
+		orbit_info.visible=true
+		orbit_name_label.text=orbit.get_planet().planet_name + " Orbit"
+		orbit_ships_label.text="Ships: "+ String(orbit.ships.size())
 
-func hide_planet_info():
+func reset_info():
 	planet_info.visible=false
-
+	orbit_info.visible=false
 
 func _on_build_button_pressed() -> void:
 	planet.build_ship()
--- entities/orbit/orbit.gd
+++ entities/orbit/orbit.gd
@@ -1,4 +1,5 @@
 tool
+class_name Orbit
 extends Node2D
 
 const orbit_speed=0.1
@@ -6,10 +7,13 @@ const orbit_click_padding=20
 
 export var radius=200
 export var orbit_color:Color
+export var highlight_color:Color
 
 export var quality:int=128
 
 var ships=[]
+
+var show_highlight=false
 
 func _ready() -> void:
 	var donut_collider:DonutCollisionPolygon2D=$Area2D/DonutCollisionPolygon2D
@@ -20,11 +24,12 @@ func _process(delta):
 		self.rotate(orbit_speed*delta)
 	update()
 
-
 func _draw():
-	draw_circle_arc(Vector2.ZERO, radius, 0, 360, orbit_color)
+	draw_circle_arc(Vector2.ZERO, radius, 0, 360, orbit_color, 1)
+	if show_highlight:
+		draw_circle_arc(Vector2.ZERO, radius, 0, 360, highlight_color, 8)
 
-func draw_circle_arc(center, radius, angle_from, angle_to, color):
+func draw_circle_arc(center, radius, angle_from, angle_to, color, width=1.0):
 	var nb_points = 16
 	var points_arc = []
 
@@ -33,8 +38,10 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 
 	for index_point in range(quality):
-		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, width)
 
+func get_planet() -> Planet:
+	return $".." as Planet
 
 func add_ship(ship) -> void:
 	ships.append(ship)
@@ -49,10 +56,11 @@ func add_ship(ship) -> void:
 
 	ship.rotation_degrees=ship_degrees
 
-
 func _on_click(button_index) -> void:
-	print("CLICK Orbit")
-
+	HUD.display_orbit_info(self)
 
 func _on_mouse_entered() -> void:
-	print("Mouse entered")
+	show_highlight=true
+
+func _on_mouse_exited():
+	show_highlight=false
